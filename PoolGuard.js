d77da17a5c4a900a73004588ef4afcc20d576ef1
wschmitt94 <wesley.schmitt94@gmail.com>
2018-11-09 17:38:38 -0700

    Implemented multiple get methods

--- PoolGuard.js
+++ PoolGuard.js
@@ -1,19 +1,53 @@
-var title = document.querySelector("h1");
-var danger = false;
-var isSafe = 'yes';
-var childName = 'bobby';
+var child_1_button = document.querySelector("#check");
+var children = document.querySelector(".children");
+var warning = document.querySelector("#warning");
+var dismiss = document.querySelector("#dismiss");
 
-if(danger == false){
-	setInterval(checkForDanger, 3000);
-	function checkForDanger() {
-		
-		fetch('http://localhost:8080/myresource').then(function(response){
+var danger = false;
+var num_children = 1;
+var status = 'yes';
+var child_name = 'bobby';
+
+function checkForDangerAll() {
+	fetch('http://localhost:8080/children').then(function(response){
 		return response.json();
 	}).then(function(myResponse){
-		isSafe = myResponse.status;
-		childName = myResponse.name;
+		for (var i = myResponse.length - 1; i >= 0; i--) {
+			CheckForDangerOne(myResponse[i]);
+		}
 	})
 }
+
+function CheckForDangerOne(child) {
+	fetch('http://localhost:8080/children/' + child).then(function(response){
+		return response.json();
+	}).then(function(myResponse){
+		status = myResponse.status;
+		child_name = myResponse.name;
+		if(status == "danger"){
+			child_name = child_name.toUpperCase();
+			danger = true;
+			var text = warning.querySelector('h2');
+			text.innerHTML = child_name + " IS IN IMMINENT PERIL";
+			warning.style.display = "block";
+		}
+	})
 }
 
+child_1_button.onclick = function() {
+	var child1 = children.querySelector("h3");
+	var child_name = child1.innerHTML;
+	CheckForDangerOne(child_name);
+	if(danger == false){
+		alert(child_name + " is safe");
+	}
+}
 
+dismiss.onclick = function() {
+	warning.style.display = "none";
+	danger = false;
+}
+
+if(danger == false){
+	setInterval(checkForDangerAll, 3000);
+}
--- server.py
+++ server.py
@@ -2,7 +2,15 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
+myChildren = ['John', 'Jake', 'Mia']
+
 class myHandler(BaseHTTPRequestHandler):
+
+	def getStatus(self, name):
+			if name == "Jared":
+				return "danger"
+			else:
+				return "safe"
 
 	def do_OPTIONS(self):
 		self.send_response(200)
@@ -12,17 +20,33 @@ class myHandler(BaseHTTPRequestHandler):
 		self.end_headers()
 
 	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-Type", "application/json")
-		self.send_header("Access-Control-Allow-Origin", "*")
-		self.end_headers()
-		x = {
-		"name": "Jake",
-		"status": "safe"
-		}
-		json_string = json.dumps(x);
-		print(json_string)
-		self.wfile.write(bytes(json_string, "utf-8"))
+		if self.path.startswith("/children/"):
+			url = self.path.split("/")
+			child = url[2]
+			self.send_response(200)
+			self.send_header("Content-Type", "application/json")
+			self.send_header("Access-Control-Allow-Origin", "*")
+			self.end_headers()
+			child_status = {
+			"name": "Not Found",
+			"status": ""
+			}
+			if child in myChildren:
+				status = self.getStatus(child)
+				child_status = {
+				"name": child,
+				"status": status
+				}
+			json_string = json.dumps(child_status);
+			self.wfile.write(bytes(json_string, "utf-8"))
+		else:
+			self.send_response(200)
+			self.send_header("Content-Type", "application/json")
+			self.send_header("Access-Control-Allow-Origin", "*")
+			self.end_headers()
+			json_string = json.dumps(myChildren);
+			self.wfile.write(bytes(json_string, "utf-8"))
+
 		
 
 def run():
